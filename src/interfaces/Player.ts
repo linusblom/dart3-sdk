1528e241a9e7ebb49f8959752df8ec97ee22bb0e
Linus Blom <linus.blom@me.com>
2020-03-15 16:49:01 +0100

    bump to 0.0.8

--- src/interfaces/Player.ts
+++ src/interfaces/Player.ts
@@ -1,22 +1,30 @@
-import { Transaction } from './Transaction';
-
 export interface Player {
-  id: string;
+  id: number;
+  account_id: string;
   name: string;
-  credits: number;
-  xp: number;
-  created: number;
-  avatarUrl: string;
+  balance: number;
+  created_at: Date;
   color: string;
-  diamonds: number;
-  played: number;
-  wins: number;
-  lost: number;
-  hits: number;
-  misses: number;
-  highest: number;
-  oneHundredEighties: number;
-  turnover: number;
-  net: number;
-  transactions: Transaction[];
+  avatar: string;
+  xp: number;
+  high_score: number;
+  one_hundred_eighties: number;
 }
+
+export interface CreatePlayer {
+  name: string;
+}
+
+export const makePlayer = (values: Partial<Player> = {}): Player => ({
+  id: 0,
+  account_id: '',
+  name: '',
+  balance: 0,
+  created_at: null,
+  color: '#FFFFFF',
+  avatar: '',
+  xp: 0,
+  high_score: 0,
+  one_hundred_eighties: 0,
+  ...values,
+});
